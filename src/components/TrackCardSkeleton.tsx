7fecb9e97a03ccc156dd35af3968a740c1e49eeb
oreterraC <diegolanseiros@gmail.com>
2026-05-24 00:08:12 +0200

    Show loading skeletons

---
+++ src/components/TrackCardSkeleton.tsx
@@ -0,0 +1,17 @@
+const TrackCardSkeleton = () => {
+  return (
+    <>
+      <div className="w-full aspect-square bg-zinc-800 rounded-lg" />
+      <div className="flex justify-start p-3">
+        <div className="w-10 h-10 rounded-full bg-zinc-700 flex-shrink-0" />
+        <div className="flex flex-col pl-3 gap-2 flex-1">
+          <h3 className="h-5 w-6/7 bg-zinc-700"></h3>
+          <span className="h-4 w-full bg-zinc-800 rounded"></span>
+          <span className="h-4 w-1/7 bg-zinc-700 rounded"></span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TrackCardSkeleton;
--- src/components/TrackGrid.tsx
+++ src/components/TrackGrid.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import TrackCardContainer from "./TrackCardContainer";
 import TrackCard from "./TrackCard";
 import type { Track } from "../types/Track";
+import TrackCardSkeleton from "./TrackCardSkeleton";
 
 const TrackGrid = () => {
   const [tracks, setTracks] = useState<Track[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   useEffect(() => {
     const controller = new AbortController();
@@ -18,13 +21,13 @@ const TrackGrid = () => {
         );
         if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
 
-        setIsLoading(false);
         const data = await response.json();
         setTracks(data);
-      } catch (error: any) {
         setIsLoading(false);
+      } catch (error: any) {
         if (error.name === "AbortError") return;
         setError(error.message);
+        setIsLoading(false);
       }
     };
 
@@ -38,14 +41,19 @@ const TrackGrid = () => {
   return (
     <>
       <div className="p-4 bg-zinc-900">
-        {isLoading && <p>Loading...</p>}
         {error && <div className="text-red-400 mb-4">{error}</div>}
         <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 bg-zinc-900 p-4">
-          {tracks?.map((track) => (
-            <TrackCardContainer key={track.id}>
-              <TrackCard track={track}></TrackCard>
-            </TrackCardContainer>
-          ))}
+          {isLoading
+            ? skeletons.map((skeleton) => (
+                <TrackCardContainer key={skeleton}>
+                  <TrackCardSkeleton />
+                </TrackCardContainer>
+              ))
+            : tracks?.map((track) => (
+                <TrackCardContainer key={track.id}>
+                  <TrackCard track={track}></TrackCard>
+                </TrackCardContainer>
+              ))}
         </div>
       </div>
     </>
